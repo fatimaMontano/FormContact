3a3fce5022a802c0fc22e422b05b7da0c03bfac2
Fátima Montano <fatima.montano16@gmail.com>
2023-04-19 08:43:36 -0600

    falta diseño de info y validaciones

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useRef } from 'react';
 import './App.css'; 
 import emailjs from '@emailjs/browser'
 import ReCAPTCHA from "react-google-recaptcha"
+import INFO from './components/info'
 
 function App() { 
 
@@ -25,30 +26,39 @@ function App() {
  };
 
    return ( 
-   <div > <p>...in the meantime, you can reach using the following form:</p> 
-     <form className='cf' ref={form} onSubmit={sendEmail}> 
-       <div> 
-         <input type='nombre' placeholder='Name' name='user_nombre' /> 
-         <input type='correo' placeholder='Email' name='user_correo' /> 
-         <input type='institucion' placeholder='Organization' name='user_institucion' /> 
-       </div> 
+    <div > 
+      <INFO></INFO>
+      <div>
+        <p>...in the meantime, you can reach using the following form:</p> 
+        <form className='cf' ref={form} onSubmit={sendEmail}> 
+        <div> 
+          <input type='nombre' placeholder='Name' name='user_nombre' /> 
+          <input type='correo' placeholder='Email' name='user_correo' /> 
+          <input type='institucion' placeholder='Organization' name='user_institucion' /> 
+        </div> 
 
-       <div> 
-         <textarea name='mensaje' type='text' placeholder='Message'></textarea> 
-       </div> 
-       <div className='recaptcha'>
-        <ReCAPTCHA
-          sitekey="6Lck65UlAAAAANh2pGeM3J3DCRmT4mZYzU5cKEZp"
-          onChange={onChange}
-          ></ReCAPTCHA>
-       </div>
-       <input type='submit' value='Enviar' id='input-submit' /> 
-     </form> 
+        <div> 
+          <textarea name='mensaje' type='text' placeholder='Message'></textarea> 
+        </div> 
 
-      <div class="footer">
-        <p>2023 DMA Research and Data Analytics | Edificio Kinétika nivel B, calle El Carmen, Santa Tecla, La Libertad, El Salvador</p>
-      </div>
-   </div> 
+        <div className='recaptcha'>
+          <ReCAPTCHA
+            sitekey="6Lck65UlAAAAANh2pGeM3J3DCRmT4mZYzU5cKEZp"
+            onChange={onChange}
+            ></ReCAPTCHA>
+        </div>
+
+        <input type='submit' value='Enviar' id='input-submit' /> 
+
+      </form> 
+
+        <div class="footer">
+          <p>2023 DMA Research and Data Analytics | Edificio Kinétika nivel B, calle El Carmen, Santa Tecla, La Libertad, El Salvador</p>
+        </div>
+
+      </div> 
+
+    </div> 
    ); 
  } 
 export default App;
