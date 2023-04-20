979e01753a6ba6546222aa4368f49a70b76c556d
Fátima Montano <fatima.montano16@gmail.com>
2023-04-20 09:53:17 -0600

    diseño

--- src/components/info.js
+++ src/components/info.js
@@ -1,10 +1,6 @@
 import React from 'react'
-import imagen1 from './1market-intelligence.png'
-import imagen2 from './2economic-research.png'
-import imagen3 from './3data-analysis.png'
-import imagen4 from './4business-intelligence.png'
-import imagen5 from './5monitoring-evaluation.png'
 import logoDMA from './logo1DMA.png'
+import CARDS from './cardgroup';
 
 const INFO = () =>
 {
@@ -19,45 +15,8 @@ const INFO = () =>
                     <h1>We support knowledge generation accross sectors and organizations</h1>
                 </div>
 
-            <div class="card-group">
-
-            <div class="card">
-                <img src={imagen1} class="card-img-top" alt="Market Intelligence icon"/>
-                <div class="card-body">
-                <h5 class="card-title">Market Intelligence</h5>
-                </div>
+                <CARDS></CARDS> 
             </div>
-
-            <div class="card">
-                <img src={imagen2} class="card-img-top" alt="Economic Research icon"/>
-                <div class="card-body">
-                <h5 class="card-title">Economic Research</h5>
-                </div>
-            </div>
-
-            <div class="card">
-                <img src={imagen3} class="card-img-top" alt="Data Analytics and Machine Learning icon"/>
-                <div class="card-body">
-                <h5 class="card-title">Data Analytics and Machine Learning</h5>
-                </div>
-            </div>
-
-            <div class="card">
-                <img src={imagen4} class="card-img-top" alt="Business Intelligence icon"/>
-                <div class="card-body">
-                <h5 class="card-title">Business Intelligence</h5>
-                </div>
-            </div>
-
-            <div class="card">
-                <img src={imagen5} class="card-img-top" alt="Monitoring and evaluation icon"/>
-                <div class="card-body">
-                <h5 class="card-title">Monitoring and evaluation</h5>
-                </div>
-            </div>
-            </div>
-
-</div>
 
         </div>
     );
